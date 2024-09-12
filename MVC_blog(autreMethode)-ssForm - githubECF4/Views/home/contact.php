1f044c1b99e5d94e3dd899ebdfe5d6211417cf46
cefii1259 <michael.legrain@hotmail.com>
2024-09-12 16:23:46 +0200

    ajoute de page de contact

---
+++ MVC_blog(autreMethode)-ssForm - githubECF4/Views/home/contact.php
@@ -0,0 +1,24 @@
+<?php $title = "Mon portfolio - Page de Contact"; ?>
+
+<form action="">
+    <fieldset>
+        <legend>Contactez-Nous</legend>
+        <label for="Nom">Nom:</label>
+        <input type="text" id="nom" name="nom">
+        <br>
+        <label for="pNom">Prenom:</label>
+        <input type="text" id="pnom" name="pnom">
+        <br>
+        <label for="Email">Email:</label>
+        <input type="Email" id="email" name="email">
+        <br>
+        <label for="comments">Comments:</label>
+        <br>
+        <textarea name="comments" id="comments" rows="10" cols="30"></textarea>
+        <br>
+    </fieldset>
+    <input type="submit" value="Envoyer">
+</form>
+<br>
+<h3>Trouvez-nous</h3>
+<img src="https://imgs.search.brave.com/geqc0XZl3CcIAxzbUfZmqscJIVO9mFKWOWn0DlCPO84/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bWFwLWZyYW5jZS5j/b20vbWFwL2ZyYW5j/ZS1tYXAuanBn" alt="France">
---
+++ MVC_blog(autreMethode)-ssForm - githubECF4/readme.txt
@@ -0,0 +1,3 @@
+- ajoute une nouvel lien pour la page contact dans base.php
+- ajoute une fonction dans homeController.php pour appeler la page contact
+- créer la page contact avec une formulaire (aucun cible) et une carte de France (pour location, placeholder)
