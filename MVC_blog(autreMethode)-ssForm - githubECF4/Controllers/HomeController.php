ca29ce541283cf5828c05da3e88068b97460c0f6
CEF1259 <149583946+CEF1259@users.noreply.github.com>
2024-09-13 14:55:05 +0200

    Update HomeController.php

--- MVC_blog(autreMethode)-ssForm - githubECF4/Controllers/HomeController.php
+++ MVC_blog(autreMethode)-ssForm - githubECF4/Controllers/HomeController.php
@@ -7,14 +7,14 @@ class HomeController extends Controller
 
     public function index()
     {
-        $creation = new Creation();
+        //$creation = new Creation(); commented out because it doesn't work
         $this->render('home/index');
     }
 
     //calls renders the contact page, function called by base.php as action
     public function contact()
     {
-        $creation = new Creation();
+        //$creation = new Creation(); commented out because it doesn't work
         $this->render('home/contact');
     }
 }
